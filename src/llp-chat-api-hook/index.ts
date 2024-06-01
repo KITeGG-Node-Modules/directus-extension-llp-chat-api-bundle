4df1736c44d701e6a673ea12c89f1f42520c8518
Francesco Scheffczyk <francesco.scheffczyk@gmail.com>
2024-06-01 16:51:01 +0200

    fix: Creation and Update of course section triggers embed of them

--- src/llp-chat-api-hook/index.ts
+++ src/llp-chat-api-hook/index.ts
@@ -1,13 +1,13 @@
 import { defineHook } from "@directus/extensions-sdk";
 
-const updateCourse = async (id: string, CHAT_API_URL: string) => {
+const updateCourse = async (id: string, type: string, CHAT_API_URL: string) => {
 	const response = await fetch(`${CHAT_API_URL}/database/post/`, {
 		method: "POST",
 		headers: {
 			"Content-Type": "application/json",
 		},
 		body: JSON.stringify({
-			type: "courses",
+			type: type,
 			id,
 		}),
 	});
@@ -23,24 +23,24 @@ export default defineHook(({ action }, { env }) => {
 	action("courses.items.create", async ({ payload }) => {
 		console.log("Creating course: ", payload);
 
-		await updateCourse(payload.id, CHAT_API_URL);
+		await updateCourse(payload.id, "courses", CHAT_API_URL);
 	});
 
 	action("courses.items.update", async ({ payload }) => {
 		console.log("Updating course: ", payload);
 
-		await updateCourse(payload.id, CHAT_API_URL);
+		await updateCourse(payload.id, "courses", CHAT_API_URL);
 	});
 
 	action("course_sections.items.create", async ({ payload }) => {
 		console.log("Creating course section: ", payload);
 
-		await updateCourse(payload.course_id.id, CHAT_API_URL);
+		await updateCourse(payload.course_id.id, "course_sections", CHAT_API_URL);
 	});
 
 	action("course_sections.items.update", async ({ payload }) => {
 		console.log("Updating course section: ", payload);
 
-		await updateCourse(payload.course_id.id, CHAT_API_URL);
+		await updateCourse(payload.course_id.id, "course_sections", CHAT_API_URL);
 	});
 });
